645a989b026f4432b780efd455c8b90793c16fd0
Fabian Roth <fabian.f.roth@daimler.com>
2017-04-27 14:38:25 -0700

    Fix bug in button state display
    -

--- example/dist/js/main.js
+++ example/dist/js/main.js
@@ -93,9 +93,9 @@ var DaydreamController = DaydreamControllerTS.DaydreamController;
 document.getElementById("connect-daydream").addEventListener("click", function () {
     var daydreamController = new DaydreamController();
     daydreamController.onStateChange(function (state) {
-        document.getElementById("state-home").innerText = "" + state.isHomeDown ? ("Pressed") : ("Not pressed");
-        document.getElementById("state-app").innerText = "" + state.isAppDown ? ("Pressed") : ("Not pressed");
-        document.getElementById("state-click").innerText = "" + state.isClickDown ? ("Pressed") : ("Not pressed");
+        document.getElementById("state-home").innerText = "" + (state.isHomeDown ? ("Pressed") : ("Not pressed"));
+        document.getElementById("state-app").innerText = "" + (state.isAppDown ? ("Pressed") : ("Not pressed"));
+        document.getElementById("state-click").innerText = "" + (state.isClickDown ? ("Pressed") : ("Not pressed"));
         document.getElementById("state-xori").innerText = "" + state.xOri;
         document.getElementById("state-yori").innerText = "" + state.yOri;
         document.getElementById("state-zori").innerText = "" + state.zOri;
--- example/main.ts
+++ example/main.ts
@@ -7,9 +7,9 @@ document.getElementById("connect-daydream").addEventListener("click", function (
 
     daydreamController.onStateChange((state: IDaydreamControllerState) => {
 
-        document.getElementById("state-home").innerText = "" + state.isHomeDown ? ("Pressed") : ("Not pressed");
-        document.getElementById("state-app").innerText = "" + state.isAppDown ? ("Pressed") : ("Not pressed");
-        document.getElementById("state-click").innerText = "" + state.isClickDown ? ("Pressed") : ("Not pressed");
+        document.getElementById("state-home").innerText = "" + (state.isHomeDown ? ("Pressed") : ("Not pressed"));
+        document.getElementById("state-app").innerText = "" + (state.isAppDown ? ("Pressed") : ("Not pressed"));
+        document.getElementById("state-click").innerText = "" + (state.isClickDown ? ("Pressed") : ("Not pressed"));
 
         document.getElementById("state-xori").innerText = "" + state.xOri;
         document.getElementById("state-yori").innerText = "" + state.yOri;
